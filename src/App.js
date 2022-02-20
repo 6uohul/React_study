f115b695e00f1c7ee81ee66e937e5463322ccf60
6uohul <kiy010604@naver.com>
2022-02-21 01:51:07 +0900

    useEffect 이론공부

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setkeyword] = useState("");
+  const onClick = () => setValue((prev) => prev +1);
+  const onChange = (event) => setkeyword(event.target.value);
+  useEffect(() => {
+    console.log("Only one time");
+  }, []);
+  useEffect(() => {
+    console.log("Only counter changes");
+  }, [counter]);
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 5){
+      console.log("Only keyword changes");
+    }
+  }, [keyword]); // keyword가 변화될 때
+  useEffect(() => {
+    console.log("keyword & counter change");
+  }, [keyword, counter]);
+  // useEffect(function (){
+  //}) "() =>" 와 같은 표현임
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <input
+        value={keyword}
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here...">
+      </input>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
