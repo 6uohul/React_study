105ef20641be70b4ac7049a787aac5673615983f
6uohul <kiy010604@naver.com>
2022-02-22 00:02:10 +0900

    Coin Tracker challenge 수정

--- src/App.js
+++ src/App.js
@@ -1,38 +1,46 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
-  //console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  const [loading, setLoading] = useState(true);
+  const [coins,setCoins] = useState([]); 
+  const [myMoney,setMyMoney] = useState(1);
+  const [need, setNeed] = useState(1);
+  const onChange = (event) => {
+    setMyMoney(event.target.value);
+    setNeed(1);
+  }
+  const handleInput = (event) => {
+    setNeed(event.target.value);
+  } 
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  },[]);
   return (
+  <div>
+    <h1>The coins! {loading ? "" :`(${coins.length} coins)`}</h1>
+    {loading ? <strong>loading...</strong> : <select onChange={onChange}>
+      <option>Select Coin!</option>
+      {coins.map((coin , index) =>
+      <option 
+        key={index} 
+        value={coin.quotes.USD.price} 
+        id={coin.symbol}
+        symbol = {coin.symbol} >
+        {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
+        </option>
+       )}
+    </select>}
+    <h2>Please write yours...</h2>
     <div>
-      <h1>My To-Do-List ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        ></input>
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <input type="number" value={need} onChange={handleInput} placeholder="dollor"/>$
     </div>
-  );
+    <hr />
+    <h2>You can get {need/myMoney}</h2>
+  </div>);
 }
-
 export default App;
