446e8a29dd388755699e9bcb13a1052c136b538f
6uohul <kiy010604@naver.com>
2022-02-24 22:07:36 +0900

    react-router-dom 5.3.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link, //브라우저 새로고침 없이 이동
+} from "react-router-dom"; 
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
 //Router를 render
 function App() {
-  return null;
+  return <Router>
+    <Switch>
+      <Route path="/movie">
+        <Detail />
+      </Route>
+      <Route path="/"> 
+        <Home />
+      </Route>
+    </Switch>
+  </Router>
 };
 export default App;
